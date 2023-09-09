f26fd77e7314182172dda5bac952c94baaafe676
TJ <tjohanseny99@gmail.com>
2023-09-09 20:13:28 +0300

    configure firebase hosting

--- src/pages/employees/Employee.js
+++ src/pages/employees/Employee.js
@@ -1,8 +1,24 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const Employee = () => {
-    return <div></div>
+    const dispatch = useDispatch();
+    const {employeeID} = useParams();
+
+    useEffect(() => {
+        const getEmployeeDetails = async() => {
+
+        }
+
+        getEmployeeDetails();
+    }, [])
+
+    return <div className="px-4">
+        <div className="w-[100%] h-[30%] rounded-lg">
+
+        </div>
+    </div>
 }
 
 export default Employee;
